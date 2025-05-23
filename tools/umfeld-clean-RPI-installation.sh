6313c82b0a2712ba311bd1d85c5e5db7cea3ca2d
dennisppaul <dennis.paul@hfk-bremen.de>
2025-05-23 09:35:51 +0200

    fixed clean up script

--- tools/umfeld-clean-RPI-installation.sh
+++ tools/umfeld-clean-RPI-installation.sh
@@ -1,15 +1,16 @@
+$USER_HOME=/home/umfeld
 sudo bash -c '
-rm -f $HOME/.bash_history $HOME/.zsh_history /root/.bash_history /root/.zsh_history
+rm -f $USER_HOME/.bash_history $USER_HOME/.zsh_history /root/.bash_history /root/.zsh_history
 rm -f /etc/ssh/ssh_host_*
 rm -f /etc/wpa_supplicant/wpa_supplicant.conf
 rm -f /var/lib/dhcp/* /etc/udev/rules.d/70-persistent-net.rules
-rm -rf $HOME/.cache/* $HOME/.local/share/* /tmp/* /var/tmp/*
+rm -rf $USER_HOME/.cache/* $USER_HOME/.local/share/* /tmp/* /var/tmp/*
 journalctl --rotate
 journalctl --vacuum-time=1s
 rm -rf /var/log/*.gz /var/log/*.1 /var/log/*.old
 truncate -s 0 /var/log/*.log
 apt clean
-rm -f $HOME/.ssh/authorized_keys
+rm -f $USER_HOME/.ssh/authorized_keys
 unset HISTFILE
 history -c
 sync

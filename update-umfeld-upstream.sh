24ed13b542334fb92f636b1a09a435ebf7560003
dennisppaul <dennis.paul@hfk-bremen.de>
2025-09-10 21:25:09 +0200

    improved push

--- update-umfeld-upstream.sh
+++ update-umfeld-upstream.sh
@@ -2,7 +2,13 @@
 
 git fetch umfeld-upstream
 git subtree pull --prefix=umfeld/cores/sdl/umfeld umfeld-upstream main --squash
-git push
+
+git branch -D umfeld-split 2>/dev/null || true
+git subtree split --prefix=umfeld/cores/sdl/umfeld -b umfeld-split
+git push umfeld-upstream umfeld-split:main
+git branch -D umfeld-split
+
+#git push
 
 # check upstream for new commits:
 # ```sh
